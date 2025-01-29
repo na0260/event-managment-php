50dd9c366997ef154d7f36e44479e5993ff360ff
na0260 <samjr.008@gmail.com>
2025-01-30 02:49:24 +0600

    navbar update

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,3 +1,8 @@
+<?php
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+?>
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container">
         <a class="navbar-brand" href="../index.php">Event Management</a>
@@ -7,8 +12,16 @@
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav ms-auto">
                 <li class="nav-item"><a class="nav-link" href="../events/list.php">Events</a></li>
-                <li class="nav-item"><a class="nav-link" href="../auth/login.php">Login</a></li>
-                <li class="nav-item"><a class="nav-link" href="../auth/register.php">Register</a></li>
+
+                <?php if (isset($_SESSION["user_id"])): ?>
+                    <?php if ($_SESSION["user_role"] == "admin"): ?>
+                        <li class="nav-item"><a class="nav-link" href="../admin/dashboard.php">Admin Panel</a></li>
+                    <?php endif; ?>
+                    <li class="nav-item"><a class="nav-link text-danger" href="../auth/logout.php">Logout</a></li>
+                <?php else: ?>
+                    <li class="nav-item"><a class="nav-link" href="../auth/login.php">Login</a></li>
+                    <li class="nav-item"><a class="nav-link" href="../auth/register.php">Register</a></li>
+                <?php endif; ?>
             </ul>
         </div>
     </div>
